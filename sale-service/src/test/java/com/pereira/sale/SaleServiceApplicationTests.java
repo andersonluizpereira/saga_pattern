250e99159aadf9166a0a60aee8e3218e133bea75
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2023-03-14 22:19:40 -0300

    feat: add domain sale

---
+++ sale-service/src/test/java/com/pereira/sale/SaleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pereira.sale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
