eec045e310852be01b90c28375fb19b6633028dd
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2023-04-15 19:54:09 -0300

    feat: created prodject orchestrator

---
+++ orchestration/orchestrator-service/src/main/java/com/pereira/orchestrator/OrchestratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pereira.orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestratorServiceApplication.class, args);
+	}
+
+}
---
+++ orchestration/orchestrator-service/src/test/java/com/pereira/orchestrator/OrchestratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pereira.orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
